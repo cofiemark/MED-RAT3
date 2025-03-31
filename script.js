ed1c5adf63a272f0714594d16a8d2a3130110283
Mark Cofie <48798962+cofiemark@users.noreply.github.com>
2025-03-31 20:34:28 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     // --- Constants and State ---
-    const GAS_URL = 'https://script.google.com/macros/s/AKfycbzRW7epRbmS0ia5jw3kIxPZCoEPSiRrH1kll1tU-F0SGnAmc81SQB2DH7iXHCe3l4oc/exec'; // <-- REPLACE THIS WITH YOUR DEPLOYED URL
+    const GAS_URL = 'https://script.google.com/macros/s/AKfycbx5YmAKeIU8PorBaT1CypHWqfUhwqjEvTx5uNA7ygAx-wI1y7ku5lfFGVoJVBgQzpwMwA/exec'; // <-- REPLACE THIS WITH YOUR DEPLOYED URL
     let allEquipment = []; // Cache for equipment data
     let allTechnicians = []; // Cache for technician data
 
